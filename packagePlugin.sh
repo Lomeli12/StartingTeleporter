e64da234478fbb9eb51f65cac46fbe97bf43b4aa
Anthony Lomeli <anthonylomeli11@gmail.com>
2023-12-10 16:38:43 -0800

    Added simple bash script to making packaging faster

---
+++ packagePlugin.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/bash
+if test -d BepInEX/; then
+  rm -rf BepInEX/
+fi 
+
+mkdir BepInEX
+mkdir BepInEX/plugins
+cp bin/Debug/netstandard2.1/StartingTeleporter.dll BepInEX/plugins/
+
+
+if [ -f StartingTeleporters.zip ]; then
+  rm -rf StartingTeleporters.zip
+fi
+
+zip -r StartingTeleporters.zip logo.png manifest.json README.md BepInEX/
+
+if test -d BepInEX/; then
+  rm -rf BepInEX/
+fi 
